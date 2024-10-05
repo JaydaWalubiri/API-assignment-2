6ac8d77ccc1fc112603ff8c949b0a3def2891065
JaydaWalubiri <166707051+JaydaWalubiri@users.noreply.github.com>
2024-10-05 22:04:35 +0300

    third commit

---
+++ register.php
@@ -0,0 +1,16 @@
+<?php
+class User {
+    private $username;
+    private $email;
+    private $phone;
+    private $password;
+
+    public function __construct($username, $email, $phone, $password) {
+        $this->username = $username;
+        $this->email = $email;
+        $this->phone = $phone;
+        $this->password = password_hash($password, PASSWORD_BCRYPT); // Hash password
+    }
+
+    
+    ?>
